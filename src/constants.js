2e3c30b7f93ac98c4ffc05ff4e155b041f422402
Sai venkatesh H <51031391+SaivenkateshHC@users.noreply.github.com>
2025-09-14 08:06:19 +0530

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,1 @@
-export const API_URL = 'https://dnhjuibb1b.execute-api.us-east-1.amazonaws.com/dev/task'
-// export const API_URL = process.env.REACT_APP_API_URL
+export const API_URL = process.env.REACT_APP_API_URL
